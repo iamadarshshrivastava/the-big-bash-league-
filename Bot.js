bf70cfc601e32f30030dd8a12b1ec58b1b046496
iamadarshshrivastava <40156673+iamadarshshrivastava@users.noreply.github.com>
2018-06-11 10:34:06 +0530

    Delete Bot.js

--- Bot.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js'); 
-constconst client = new Discord.Client(); 
-client.on('ready', () => 
-{ console.log(`Logged in as $
-{client.user.tag}!`); }); 
-client.on('message', msg => { if 
-(msg.content === 'ping') { msg.reply('Pong!'); } }); 
-client.login(process.env.Bot_token);
